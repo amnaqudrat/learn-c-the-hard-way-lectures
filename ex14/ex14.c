04aa0aaa6ba79bab8b180c99dcd1785eb3f1eb46
amnaqudrat <amnaqudrat@outlook.com>
2016-10-16 19:14:35 +0500

    Update ex14.c

--- ex14/ex14.c
+++ ex14/ex14.c
@@ -1,19 +1,24 @@
-#include <stdio.h>
-#include <ctype.h>
+#include <stdio.h>// standerd input output library for several functions
+#include <ctype.h> // c standerd function
 
 // forward declarations
-int can_print_it(char ch);
-void print_letters(char arg[]);
+int can_print_it(char ch); // function return interger value can print character/parameters
+void print_letters(char arg[]);// the function/mthod return nothing but print letters/characters in arg array
 
 void print_arguments(int argc, char *argv[])
 {
-    int i = 0;
+    // int argc => integer value which is count of aeguments/parameters
+    // char *argv[] => pointer to character array which store argument value pass to functio/method and ends at '\0' or NULL 
+    int i = 0; // local variable i declare and intialize with 0 in function/method
 
     for (i = 0; i < argc; i++) {
+    //for loop until argc value which is 4 in my case so for loop will loop until 3 is found
+    // this function/methods print characters letters passed to it in array of index i    
         print_letters(argv[i]);
     }
 }
 
+// print letters function defination start here
 void print_letters(char arg[])
 {
     int i = 0;
